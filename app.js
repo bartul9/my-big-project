734973095ae855035be7c0466e3517e7d84958db
bartul_9 <bartul123@outlook.com>
2021-01-04 00:32:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -23,3 +23,13 @@ function initMap() {
 $(window).scroll(function () {
   $("nav").toggleClass("scrolled", $(this).scrollTop() > 50);
 });
+
+// Div with links, night day
+
+// const divImg = document.querySelector(".hero");
+
+// divImg.addEventListener("click", function () {
+//   window.open(
+//     "https://www.google.com/search?q=new+york+day&rlz=1C1CHBD_enHR925HR925&sxsrf=ALeKk03hg1EenW1FfEXLDf5bE-gx5hFWyg:1609712255736&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiY9sW55YDuAhWjtYsKHY3HClQQ_AUoAXoECBQQAw&biw=1366&bih=625"
+//   );
+// });
